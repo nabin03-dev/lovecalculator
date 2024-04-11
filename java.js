4f029bddd06773b31f2f4e9ad3a478efa794555f
nabin03-dev <69096371+nabin03-dev@users.noreply.github.com>
2024-04-11 10:05:18 +0545

    Add files via upload

--- java.js
+++ java.js
@@ -3,40 +3,34 @@
 let isNotClicked = true;
 
 function loveCalculator() {
-    const yourName = document.getElementById("firstname").value;
-    const loversName = document.getElementById("lovername").value;
-    let loveScore = Math.random() * 100;
-    let loveFinal = Math.round(loveScore);
+    const yourName = document.getElementById("firstname").value.toLowerCase();
+    const loversName = document.getElementById("lovername").value.toLowerCase();
 
+    // Special case
+    if ((yourName === "aayushaa" && loversName === "nabin") || (yourName === "nabin" && loversName === "aayushaa")) {
+        // Set love score to 10000% for special case
+        var loveFinal = 10000;
+    } else {
+        // Calculate a random love score
+        let loveScore = Math.random() * 100;
+        var loveFinal = Math.round(loveScore);
+    }
+
+    // Check if names are empty
     if (yourName === "") {
         alert("Please enter your name.");
     } else if (loversName === "") {
         alert("Please enter your lover's name.");
-    } else if (isNotClicked) {
-        // Special case: Set loveFinal to 100% if either (yourName = aayushaa and loversName = nabin) or (yourName = nabin and loversName = aayushaa)
-        if ((yourName === "aayushaa" && loversName === "nabin") || (yourName === "nabin" && loversName === "aayushaa")) {
-            loveFinal = 10000;
-        }
-        
-        // Set the love score text
+    } else {
+        // Display the love score
         const loveFinalElement = document.getElementById("lovefinal");
-        loveFinalElement.innerHTML = `${yourName} and ${loversName}, your love score is ${loveFinal}%`;
+        loveFinalElement.innerHTML = `${yourName.charAt(0).toUpperCase() + yourName.slice(1)} and ${loversName.charAt(0).toUpperCase() + loversName.slice(1)}, your love score is ${loveFinal}%`;
+
 
         // Add the blinking class
         loveFinalElement.classList.add("blinking");
-        
+
         // Automatically remove the blinking class after 5 seconds
-        setTimeout(() => {
-            loveFinalElement.classList.remove("blinking");
-        }, 5000);
-        
-        isNotClicked = false;
-    } else {
-        // If the function is called multiple times
-        const loveFinalElement = document.getElementById("lovefinal");
-        loveFinalElement.innerHTML = `${yourName} and ${loversName}, your love score is ${loveFinal}%`;
-        
-        loveFinalElement.classList.add("blinking");
         setTimeout(() => {
             loveFinalElement.classList.remove("blinking");
         }, 5000);
